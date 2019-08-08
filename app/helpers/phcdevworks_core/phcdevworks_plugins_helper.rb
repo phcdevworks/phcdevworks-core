d5409500dfa066d29d230b8fcbb2719a5ed22bab
BradPotts <brad.potts@phcnet.co>
2019-08-08 10:32:49 +0000

    file fixes and updated admin theme.

--- app/helpers/phcdevworks_core/phcdevworks_plugins_helper.rb
+++ app/helpers/phcdevworks_core/phcdevworks_plugins_helper.rb
@@ -1,39 +1,39 @@
 module PhcdevworksCore
-    module PhcdevworksPluginsHelper
-    
-        # PHCDevworks - Accounts - Add Admin Only Filter
-        def phcdevworks_accounts_admin_only
-            unless current_user && current_user.admin?
-                redirect_to main_app.root_path, :flash => { :error => "Sorry Access Denied. Adminisistration Access Required!" }
-            end
-        end
-
-        # PHCDevworks - Accounts - Who Dunnit
-        def user_for_paper_trail
-            current_user ? current_user.username : 'Public user'
-        end
-
-        # PHCDevworks - Members - Grab Member Information
-        def phcmembers_get_member_profile_info
-            @members_profile_info = Phcdevworksmembers::Member::Profile.find(params[:profile_id])
-        end
-
-        # PHCDevworks - RealEstate - All Listings
-        def phcdevworks_real_listings_all
-            @perty_real_listings__all = Phcdevworksrealestate::Property::Listing.all
-        end
-
-        # PHCDevworks - RealEstate - Resolve Layouts
-        def resolve_property_listing_layouts
-            case action_name
-                when "new", "create"
-                    "application"
-                when "index"
-                    "phcdevworks_real_estate/property_list"
-                else
-                    "application"
-            end
-        end
-
+  module PhcdevworksPluginsHelper
+  
+    # PHCDevworks - Accounts - Add Admin Only Filter
+    def phcdevworks_accounts_admin_only
+      unless current_user && current_user.admin?
+        redirect_to main_app.root_path, :flash => { :error => "Sorry Access Denied. Adminisistration Access Required!" }
+      end
     end
+
+    # PHCDevworks - Accounts - Who Dunnit
+    def user_for_paper_trail
+      current_user ? current_user.username : 'Public user'
+    end
+
+    # PHCDevworks - Members - Grab Member Information
+    def phcmembers_get_member_profile_info
+      @members_profile_info = Phcdevworksmembers::Member::Profile.find(params[:profile_id])
+    end
+
+    # PHCDevworks - RealEstate - All Listings
+    def phcdevworks_real_listings_all
+      @perty_real_listings__all = Phcdevworksrealestate::Property::Listing.all
+    end
+
+    # PHCDevworks - RealEstate - Resolve Layouts
+    def resolve_property_listing_layouts
+      case action_name
+        when "new", "create"
+          "application"
+        when "index"
+          "phcdevworks_real_estate/property_list"
+        else
+          "application"
+      end
+    end
+
+  end
 end
--- phcdevworks_core.gemspec
+++ phcdevworks_core.gemspec
@@ -27,8 +27,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "rails", "~> 6.0.0.rc2"
 
   # Theme Dependencies
-  spec.add_dependency "phcthemes_admin_panel_pack", "~> 0.27.0"
-  spec.add_dependency "phcthemes_web_theme_pack", "~> 0.14.0"
+  spec.add_dependency "phcthemes_admin_panel_pack", "~> 0.29.0"
+  spec.add_dependency "phcthemes_web_theme_pack", "~> 0.15.0"
 
   # Development Dependencies
   spec.add_development_dependency "sqlite3", "~> 1.4", ">= 1.4.1"
